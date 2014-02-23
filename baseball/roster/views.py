f8d666e1ba122437bf5955168753591dcf22ea64
josieh <hollingsworth.josie@gmail.com>
2014-02-23 17:04:54 -0500

    adding baseball

---
+++ baseball/roster/views.py
@@ -0,0 +1,11 @@
+# project1/baseball/roster/views.py
+
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+
+def home (request):
+    return render(request, "base.html")
+    
