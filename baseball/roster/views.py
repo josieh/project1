c8c9b83cafac572cf19499f907addfa245301a55
josieh <hollingsworth.josie@gmail.com>
2014-03-03 02:49:36 -0500

    commit for the ages, before have added coaches

--- baseball/roster/views.py
+++ baseball/roster/views.py
@@ -1,5 +1,5 @@
 # project1/baseball/roster/views.py
-from roster.models import Player, Coach
+from roster.models import Coach, Player
 from django.shortcuts import render, get_object_or_404, redirect, render_to_response
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
@@ -12,7 +12,7 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 def home (request):
     context= {
         'message' : " ",
-        'player_count': Player.objects.count(),
+        'player_count': Player.objects.count()
     }
     return render(request, 'roster/home.html', context)
     
@@ -33,14 +33,34 @@ def roster (request):
     except EmptyPage:
         players = paginator.page(1)
     return render(request, "roster/player_list.html", {'players': players})
+    
+    
 
 
 #detail page 
 def player (request, pk):
     player = get_object_or_404(Player, id=pk)
     return render(request, "roster/detail.html", {'player': player})
-    coach = Coach.objects.all()
-    return render(request, "roster/detail.html", {"coaches":coach})
     
 
+
+    
+def coach(request,pk):
+	coach = get_object_or_404(Coach, id=pk)
+	return render(request, "roster/coach.html", {"coach":coach})
+
+def coachList(request):
+	coach_list = Coach.objects.all()
+	paginator = Paginator(coach_list, 100)
+	page = request.GET.get('page')
+	try:
+		coaches=paginator.page(page)
+	except PageNotAnInteger:
+		#if page is not an integer, deliver first page.
+		coaches=paginator.page(1)
+	except EmptyPage:
+		#i f page is out of range (eg 9000), deliver last page of results.
+		coaches = paginator.page(paginator.num_pages)
+
+	return render(request, 'roster/coach_list.html', {"coaches":coaches})
 
