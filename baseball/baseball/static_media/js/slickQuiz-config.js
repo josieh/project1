67b3574b9f3f87865a925397d8cc83a125c435da
josieh <hollingsworth.josie@gmail.com>
2014-03-03 04:40:51 -0500

    quiz working

--- baseball/baseball/static_media/js/slickQuiz-config.js
+++ baseball/baseball/static_media/js/slickQuiz-config.js
@@ -4,69 +4,69 @@
 
 var quizJSON = {
     "info": {
-        "name":    "Test Your Knowledge!!",
-        "main":    "<p>Think you're smart enough to be on Jeopardy? Find out with this super crazy knowledge quiz!</p>",
+        "name":    "Test Your Baseball Knowledge!!",
+        "main":    "<p>Test your knowledge of current and historical sports knowledge </p>",
         "results": "<h5>Learn More</h5><p>Etiam scelerisque, nunc ac egestas consequat, odio nibh euismod nulla, eget auctor orci nibh vel nisi. Aliquam erat volutpat. Mauris vel neque sit amet nunc gravida congue sed sit amet purus.</p>",
-        "level1":  "Jeopardy Ready",
-        "level2":  "Jeopardy Contender",
-        "level3":  "Jeopardy Amateur",
-        "level4":  "Jeopardy Newb",
-        "level5":  "Stay in school, kid..." // no comma here
+        "level1":  "Baseball Trivia-Night Ready",
+        "level2":  "Trivia Contender",
+        "level3":  "Diamond Amateur",
+        "level4":  "Trivia Beginner",
+        "level5":  "Hit the books, you'll get there" // no comma here
     },
     "questions": [
         { // Question 1 - Multiple Choice, Single True Answer
-            "q": "What number is the letter A in the English alphabet?",
+            "q": "What year was the first official game of baseball played?",
             "a": [
-                {"option": "8",      "correct": false},
-                {"option": "14",     "correct": false},
-                {"option": "1",      "correct": true},
-                {"option": "23",     "correct": false} // no comma here
+                {"option": "1876",      "correct": false},
+                {"option": "1493",     "correct": false},
+                {"option": "1910",      "correct": false},
+                {"option": "1846",     "correct": true} // no comma here
             ],
-            "correct": "<p><span>That's right!</span> The letter A is the first letter in the alphabet!</p>",
-            "incorrect": "<p><span>Uhh no.</span> It's the first letter of the alphabet. Did you actually <em>go</em> to kindergarden?</p>" // no comma here
+            "correct": "<p><span>That's right!</span> The first offical game of baseball was played in 1846, before the Civil War!</p>",
+            "incorrect": "<p><span>Incorrect</span> The first offical game of baseball was played in 1846, before the Civil War!</p>" // no comma here
         },
         { // Question 2 - Multiple Choice, Multiple True Answers, Select Any
-            "q": "Which of the following best represents your preferred breakfast?",
+            "q": "Who is the head coach of the Tar Heels?",
             "a": [
-                {"option": "Bacon and eggs",               "correct": false},
-                {"option": "Fruit, oatmeal, and yogurt",   "correct": true},
-                {"option": "Leftover pizza",               "correct": false},
-                {"option": "Eggs, fruit, toast, and milk", "correct": true} // no comma here
+                {"option": "Matt Williams",               "correct": false},
+                {"option": "Mike Fox",                   "correct": true},
+                {"option": "Mike Rizzo",               "correct": false},
+                {"option": "Dave Arendas", "correct": false} // no comma here
             ],
             "select_any": true,
-            "correct": "<p><span>Nice!</span> Your cholestoral level is probably doing alright.</p>",
-            "incorrect": "<p><span>Hmmm.</span> You might want to reconsider your options.</p>" // no comma here
+            "correct": "<p><span>Nice!</span> Mike Fox, graduate of the University of North Carolina, is in his 15th season as head coach.</p>",
+            "incorrect": "<p><span> Incorrect. </span> Mike Fox, graduate of the University of North Carolina, is in his 15th season as head coach.</p>" // no comma here
         },
         { // Question 3 - Multiple Choice, Multiple True Answers, Select All
-            "q": "Where are you right now? Select ALL that apply.",
+            "q": "How much did the average baseball ticket cost in 1920?",
             "a": [
-                {"option": "Planet Earth",           "correct": true},
-                {"option": "Pluto",                  "correct": false},
-                {"option": "At a computing device",  "correct": true},
-                {"option": "The Milky Way",          "correct": true} // no comma here
+                {"option": "$1",           "correct": true},
+                {"option": "50 cents",                  "correct": false},
+                {"option": "$3",  "correct": false},
+                {"option": "All games were free back then",          "correct": false} // no comma here
             ],
-            "correct": "<p><span>Brilliant!</span> You're seriously a genius, (wo)man.</p>",
-            "incorrect": "<p><span>Not Quite.</span> You're actually on Planet Earth, in The Milky Way, At a computer. But nice try.</p>" // no comma here
+            "correct": "<p><span>Brilliant!</span> The average baseball ticket cost $1.00 in 1920. Booming 20s?</p>",
+            "incorrect": "<p><span>Not Quite.</span> The average baseball ticket cost $1.00 in 1920. Booming 20s?</p>" // no comma here
         },
         { // Question 4
-            "q": "How many inches of rain does Michigan get on average per year?",
+            "q": "What year was the first TV broadcast of a baseball game?",
             "a": [
-                {"option": "149",    "correct": false},
-                {"option": "32",     "correct": true},
-                {"option": "3",      "correct": false},
-                {"option": "1291",   "correct": false} // no comma here
+                {"option": "1950",    "correct": false},
+                {"option": "1939",     "correct": true},
+                {"option": "1945",      "correct": false},
+                {"option": "1921",   "correct": false} // no comma here
             ],
-            "correct": "<p><span>Holy bananas!</span> I didn't actually expect you to know that! Correct!</p>",
-            "incorrect": "<p><span>Fail.</span> Sorry. You lose. It actually rains approximately 32 inches a year in Michigan.</p>" // no comma here
+            "correct": "<p><span>Correct!</span> The first televised baseball game in the United States was 1939.</p>",
+            "incorrect": "<p><span>Nope</span> The first televised baseball game in the United States was 1939.</p>" // no comma here
         },
         { // Question 5
-            "q": "Is Earth bigger than a basketball?",
+            "q": "Go Heels?",
             "a": [
                 {"option": "Yes",    "correct": true},
                 {"option": "No",     "correct": false} // no comma here
             ],
-            "correct": "<p><span>Good Job!</span> You must be very observant!</p>",
-            "incorrect": "<p><span>ERRRR!</span> What planet Earth are <em>you</em> living on?!?</p>" // no comma here
+            "correct": "<p><span>Correct</span>You've come to the right place. </p>",
+            "incorrect": "<p><span>ERRRR!</span>You can leave now. </p>" // no comma here
         } // no comma here
     ]
 };
